1b66db78bc1592593ae68cd42d7f0cc4a8117d40
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-01-09 15:07:20 +0600

    added configuration and connection to the db

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class TypeOrmConfig implements TypeOrmOptionsFactory {
+  constructor(private readonly configService: ConfigService) {}
+
+  public createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('POSTGRES_HOST'),
+      port: this.configService.get<number>('POSTGRES_PORT'),
+      username: this.configService.get<string>('POSTGRES_USERNAME'),
+      password: this.configService.get<string>('POSTGRES_PASSWORD'),
+      database: this.configService.get<string>('POSTGRES_DATABASE'),
+      synchronize: true,
+      autoLoadEntities: true,
+    };
+  }
+}
