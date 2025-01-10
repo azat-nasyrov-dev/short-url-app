8ef4bdea1e1ccc0e97a9757b54c6f413a130a41f
Azat Nasyrov <azat.nasyrov31@gmail.com>
2025-01-10 20:08:58 +0600

    added dto and validation to it

---
+++ src/urls/dto/create-short-url.dto.ts
@@ -0,0 +1,16 @@
+import { IsDate, IsNotEmpty, IsOptional, IsString, MaxLength } from 'class-validator';
+
+export class CreateShortUrlDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly originalUrl: string;
+
+  @IsOptional()
+  @IsString()
+  @MaxLength(20, { message: 'Custom alias must not exceed 20 characters' })
+  readonly customAlias?: string;
+
+  @IsOptional()
+  @IsDate()
+  readonly expiresAt?: Date;
+}
